09abfbf9f181ec0849d9f16549eb3f9e64aedad8
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-10-05 14:58:50 -0700

    Initialize repo

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.listen(port, () => {
+  console.log(`Bluefin listening at http://localhost:${port}`);
+});
